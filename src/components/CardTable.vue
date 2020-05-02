5ef10843f2e30e486312ad26b3c7c5c25277aa4a
Craig Barratt <craigtbarratt@gmail.com>
2020-05-03 00:56:13 +0100

    logic added

--- src/components/CardTable.vue
+++ src/components/CardTable.vue
@@ -8,7 +8,7 @@
     </div>
     <div class="cards-container">
     <div class="cards">
-      <img class="card" :src="getCard()">
+      <img class="card" :src="start ? getCard() : back">
     </div>
     </div>
     <div class="button-container">
@@ -21,6 +21,7 @@
 import back from '@/assets/cards/back.png'
 import button from '@/assets/button.png'
 import {CardsArray} from '@/config/CardsArray.js'
+import {RestartArray} from '@/config/RestartArray.js'
 
 export default {
   name: 'card-table',
@@ -30,26 +31,66 @@ export default {
       'button': button,
       'playerCall': 1,
       'cardsRemaining': 52,
+      'start': null,
+      'cardsArray': CardsArray,
+      'restartArray': RestartArray,
       'card': null,
-      'cardsArray': CardsArray
+      'cardKey': null,
+      'cardValue': null
     }
   },
   methods: {
     handleClick(){
-      this.playerCall +=1;
-      this.cardsRemaining -=1;
-      this.removeCard()
+      this.start = true,
+      this.getCardObject(),
+      this.getCardKey(),
+      this.getCardValue(),
+      this.pablosDasPatience()
     },
     getCard() {
-      return require('@/assets/cards/' + this.cardsArray[this.randomCard(this.cardsRemaining)] + '.png');
+      return require('@/assets/cards/' + this.cardKey + '.png');
+    },
+    getCardObject() {
+      this.card = this.cardsArray[this.randomCard(this.cardsRemaining)]
+    },
+    getCardKey() {
+      this.cardKey = Object.keys(this.card)
+    },
+    getCardValue() {
+      this.cardValue = Object.values(this.card)[0]
+    },
+    randomCard(num) {
+      return Math.floor(Math.random() * (num))
+    },
+    pablosDasPatience() {
+      if (this.cardsRemaining === 0)
+      {
+        alert('YOU WON!');
+      } else if (this.cardsRemaining > 0 && this.playerCall !== this.cardValue) {
+        this.playerCallCap()
+        this.decreaseCardsRemaining()
+        this.removeCard()
+      } else {
+        alert('YOU LOSE!');
+        this.playerCall = 1;
+        this.cardsRemaining = 52;
+        this.start = null;
+        this.card = null;
+        this.cardKey = null;
+        this.cardValue = null;
+        this.cardsArray = [...this.restartArray];
+      }
+    },
+    playerCallCap() {
+      this.playerCall >= 13 ? this.playerCall = 1 : this.playerCall++
+    },
+    decreaseCardsRemaining() {
+      this.cardsRemaining -= 1;
     },
     removeCard() {
       const index = this.cardsArray.indexOf(this.getCard());
       this.cardsArray.splice(index, 1)
     },
-    randomCard(num) {
-      return Math.floor(Math.random() * (num))
-    }
   }
 }
 </script>
