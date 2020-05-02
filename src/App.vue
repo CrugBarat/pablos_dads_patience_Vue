07114653e75b4347953d8d4906a705ca54f734d7
Craig Barratt <craigtbarratt@gmail.com>
2020-05-02 22:30:41 +0100

    adds background, font, card files

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template lang="html">
-
+  <div>
+    <div class="title">
+      <p>Pablo's Da's Patience</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,4 +12,32 @@ export default {
 </script>
 
 <style lang="css" scoped>
+.title {
+  font-family: 'blackjack';
+  color: white;
+  text-align: center;
+  font-size: 8vw;
+  opacity: 80%;
+}
+
+p {
+  margin: 0;
+  padding: 0;
+}
+</style>
+
+<style>
+body {
+  background-image: url('assets/background.png');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+  background-size: cover;
+}
+
+@font-face {
+  font-family: 'blackjack';
+  src: url('./assets/fonts/blackjack.otf');
+}
+
 </style>
